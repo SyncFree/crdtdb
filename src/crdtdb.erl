5b5eb6ef273a63d3cd4fd516cedf26aa26883b96
Russell Brown <russell.brown@mac.com>
2014-01-09 18:25:49 +0000

    Tutorial part 2 - the vnode
    Make a basic key->crdt database

--- src/crdtdb.erl
+++ src/crdtdb.erl
@@ -3,12 +3,13 @@
 -include_lib("riak_core/include/riak_core_vnode.hrl").
 
 -export([
-         ping/0
+         ping/0,
+         get/1,
+         put/3
         ]).
 
--ignore_xref([
-              ping/0
-             ]).
+-define(BUCKET, <<"crdtdb">>).
+
 
 %% Public API
 
@@ -18,3 +19,20 @@ ping() ->
     PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, crdtdb),
     [{IndexNode, _Type}] = PrefList,
     riak_core_vnode_master:sync_spawn_command(IndexNode, ping, crdtdb_vnode_master).
+
+get(Key) ->
+    DocIdx = riak_core_util:chash_key({?BUCKET, Key}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, crdtdb),
+    [{IndexNode, _Type}] = PrefList,
+    case crdtdb_vnode:get(IndexNode, Key) of
+        {ok, #crdt{mod=M, value=V}} ->
+            {ok, {M, V}};
+        E ->
+            E
+    end.
+
+put(Key, Mod, Value) ->
+    DocIdx = riak_core_util:chash_key({?BUCKET, Key}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, crdtdb),
+    [{IndexNode, _Type}] = PrefList,
+    crdtdb_vnode:put(IndexNode, Key, #crdt{mod=Mod, value=Value}).
